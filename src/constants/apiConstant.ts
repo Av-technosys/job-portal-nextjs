a86c586e5763fd6153de00e3f135b267c096e374
Ravindra Singh <159220393+ravindrasingh777@users.noreply.github.com>
2025-09-19 14:58:45 +0530

    Update apiConstant.ts

--- src/constants/apiConstant.ts
+++ src/constants/apiConstant.ts
@@ -81,6 +81,7 @@ export const apiConstantsURL = {
     getQuestionsBySubjectId: `assessment/get_test_by_subject_id/`,
     assessmentAnsweredData: `assesment/submit_test_by_id/`,
     deleteQuestion: `assessment/delete_question/`,
+    getQuestionBySubjectId: `assessment/get_question_by_subject_id/`,
     getQuestionById: `assessment/get_question_by_id/`,
   },
 };
